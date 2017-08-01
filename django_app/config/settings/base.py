cd3715184aae85424cce967c07b2a2f394b15ba0
Hyeongjin Kim <hyeongjin184@gmail.com>
2017-08-01 16:26:13 +0900

    DATABASE debug, deploy 분리

--- django_app/config/settings/base.py
+++ django_app/config/settings/base.py
@@ -81,15 +81,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
--- django_app/config/settings/deploy.py
+++ django_app/config/settings/deploy.py
@@ -13,5 +13,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 DEBUG = False
 ALLOWED_HOSTS = config_secret_deploy['django']['allowed_hosts']
 
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = config_secret_deploy['django']['databases']
+
 print('===== DEBUG', DEBUG)
 print('===== ALLOWED_HOSTS', ALLOWED_HOSTS)
