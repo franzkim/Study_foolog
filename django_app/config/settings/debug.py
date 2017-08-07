53461aacceca7f6cbb32d850cb3093327c1fd7c9
Hyeongjin Kim <hyeongjin184@gmail.com>
2017-08-07 14:25:49 +0900

    eb

--- django_app/config/settings/debug.py
+++ django_app/config/settings/debug.py
@@ -3,6 +3,7 @@ from .base import *
 
 config_secret_debug = json.loads(open(CONFIG_SECRET_DEBUG_FILE).read())
 
+# WSGI application
 WSGI_APPLICATION = 'config.wsgi.debug.application'
 
 # Static URLs
